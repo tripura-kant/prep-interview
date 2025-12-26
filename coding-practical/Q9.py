d75aee802b697415770efb60cbf3f36c4ce66480
Tripura kant <tripura.kant@zoom.us>
2025-12-26 08:56:58 +0530

    Fri Dec 26 08:56:58 IST 2025

--- coding-practical/Q9.py
+++ coding-practical/Q9.py
@@ -5,3 +5,11 @@ Log:
  Task: Extract the referrer field and show the top 10 sources.
 '''
 
+log_file = "/Users/tripurakant/Documents/code/My_git/prep-int/coding-practical/access.log"
+result = 0
+count = 0
+with open(log_file) as f:
+    for line in f:
+        parts = line.split('"')
+        ref = parts[1]
+        print(ref)
